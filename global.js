8854efdc226e5266ddf9bdf83925a7580371be15
James Bennett <JamesBennett25565@gmail.com>
2021-09-02 23:49:40 +0100

    initial commit

---
+++ global.js
@@ -0,0 +1,7 @@
+// Add header to page
+document.write(`\
+<div class="header">\
+    <button class="navButton" onclick="window.location.href='/'">Home</button>
+    <button class="navButton" onclick="window.location.href='/pages/misc/myPcSpecs.html'">My PC Specs</button>
+</div>\
+`);
