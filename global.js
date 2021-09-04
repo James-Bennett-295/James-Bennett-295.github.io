3029032f68c8001d8a4695c95721e502486bbbac
James Bennett <JamesBennett25565@gmail.com>
2021-09-04 20:40:10 +0100

    added linux minecraft server tutorial

--- global.js
+++ global.js
@@ -3,5 +3,6 @@ document.write(`\
 <div class="header">\
     <button class="navButton" onclick="window.location.href='/'">Home</button>
     <button class="navButton" onclick="window.location.href='/pages/misc/myPcSpecs.html'">My PC Specs</button>
+    <button class="navButton" onclick="window.location.href='/pages/tutorials/tutorialsIndex.html'">Tutorials</button>
 </div>\
 `);
