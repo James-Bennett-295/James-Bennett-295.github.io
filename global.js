17369a32b59bffb992c34786e2a26e251419c1c7
James Bennett <JamesBennett25565@gmail.com>
2021-10-04 16:16:24 +0100

    renamed "navButton" to "button1" in global.css

--- global.js
+++ global.js
@@ -1,8 +1,9 @@
 // Add header to page
 document.write(`\
 <div class="header">\
-    <button class="navButton" onclick="window.location.href='/'">Home</button>
-    <button class="navButton" onclick="window.location.href='/pages/misc/myPcSpecs.html'">My PC Specs</button>
-    <button class="navButton" onclick="window.location.href='/pages/tutorials/tutorialsIndex.html'">Tutorials</button>
+    <button class="button1" onclick="window.location.href='/'">Home</button>
+    <button class="button1" onclick="window.location.href='/pages/misc/myPcSpecs.html'">My PC Specs</button>
+    <button class="button1" onclick="window.location.href='/pages/tutorials/tutorialsIndex.html'">Tutorials</button>
+    <button class="button1" onclick="window.location.href='/pages/tools/toolsIndex.html'">Tools</button>
 </div>\
 `);
